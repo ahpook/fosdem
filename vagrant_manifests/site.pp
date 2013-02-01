378688f4238fcee26027922e650022486b00c9af
Eric Sorenson <eric.sorenson@puppetlabs.com>
2013-02-01 05:48:19 -0800

    style fixups

--- vagrant_manifests/site.pp
+++ vagrant_manifests/site.pp
@@ -2,10 +2,10 @@
 node default {
 
   # set up name resolution so we can get to the master
-  host { "puppet":
-    ensure => present,
-    host_aliases => [ "glitched","glitched.local" ],
-    ip => "192.168.23.10",
+  host { 'puppet':
+    ensure       => present,
+    host_aliases => [ 'glitched','glitched.local' ],
+    ip           => '192.168.23.10',
   }
 
 }
